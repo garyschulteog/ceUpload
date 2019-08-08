618dbe890b150e39774f9597a1b367d3424e185c
Gary Schulte <gary.schulte@opengov.com>
2019-08-08 16:46:13 -0700

    first pass at wiring up to dataset cost element controller

--- ceLookupTableUpload.go
+++ ceLookupTableUpload.go
@@ -31,8 +31,35 @@ type keycloakCreds struct {
 	Password     string `json:"password"`
 }
 
+type CostElementResult struct {
+	sheetName   string
+	env         string
+	workforceId string
+	costElement CostElement
+	errors      []error
+}
+
+type CostElementResponse struct {
+	body string
+	err  error
+}
+
+var schemaLoader = gojsonschema.NewReferenceLoader("file://./cost-element.json")
+
 // Retrieve a token, saves the token, then returns the generated client.
-func getClient(config *oauth2.Config) *http.Client {
+func getClient() *http.Client {
+	// get google api creds from file
+	b, err := ioutil.ReadFile("credentials.json")
+	if err != nil {
+		log.Fatalf("Unable to read client secret file: %v", err)
+	}
+
+	// If modifying these scopes, delete your previously saved token.json.
+	config, err := google.ConfigFromJSON(b, "https://www.googleapis.com/auth/spreadsheets.readonly")
+	if err != nil {
+		log.Fatalf("Unable to parse client secret file to config: %v", err)
+	}
+
 	// The file token.json stores the user's access and refresh tokens, and is
 	// created automatically when the authorization flow completes for the first
 	// time.
@@ -191,13 +218,18 @@ func (e *parseError) Error() string {
 	return fmt.Sprintf(e.prob)
 }
 
-func handleSheet(s *sheets.Sheet) (*CostElement, []error) {
+func handleSheet(s *sheets.Sheet) CostElementResult {
+	ceResult := CostElementResult{}
+	ceResult.sheetName = s.Properties.Title
 	rows := s.Data[0].RowData
+	ceResult.workforceId = rows[1].Values[1].FormattedValue
 
-	if rows[2].Values[1].FormattedValue != "opengov.com" {
-		return nil, []error{&parseError{
+	ceResult.env = rows[2].Values[1].FormattedValue
+	if ceResult.env != "opengov.com" && ceResult.env != "ogintegration.us" {
+		ceResult.errors = []error{&parseError{
 			fmt.Sprintf("env '%s' not a candidate for cost element",
-				rows[3].Values[1].FormattedValue)}}
+				rows[2].Values[1].FormattedValue)}}
+		return ceResult
 	}
 	// log.Printf("Building Cost Element : %s\n", rows[3].Values[1].FormattedValue)
 	// build a Configuration
@@ -205,21 +237,20 @@ func handleSheet(s *sheets.Sheet) (*CostElement, []error) {
 	sourceDetails := CostElementSource("TABLE")
 	valueTypeDetails := CostElementValueType(strings.ToUpper(rows[9].Values[1].FormattedValue))
 	createdAt := CreatedAt(time.Now().Format(time.RFC3339))
-	ce := CostElement{
+	ceResult.costElement = CostElement{
 		Configuration: conf,
 		CreatedAt:     &createdAt,
 		Source:        sourceDetails,
 		Template:      CostElementTemplate("NONE"),
 		ValueType:     valueTypeDetails}
-	// shit, _ := json.MarshalIndent(ce, "", "\t")
-	// fmt.Printf(string(shit))
-	return validateCostElement(&ce)
+	// tmp, _ := json.MarshalIndent(ce, "", "\t")
+	// fmt.Printf(string(tmp))
+	validateCostElementResult(&ceResult)
+	return ceResult
 }
 
-var schemaLoader = gojsonschema.NewReferenceLoader("file://./cost-element.json")
-
-func validateCostElement(ce *CostElement) (*CostElement, []error) {
-	json, _ := json.MarshalIndent(&ce, "", "\t")
+func validateCostElementResult(ceResult *CostElementResult) {
+	json, _ := json.MarshalIndent(ceResult.costElement, "", "\t")
 	documentLoader := gojsonschema.NewStringLoader(string(json))
 	result, err := gojsonschema.Validate(schemaLoader, documentLoader)
 	if err != nil {
@@ -230,11 +261,11 @@ func validateCostElement(ce *CostElement) (*CostElement, []error) {
 	for _, err := range result.Errors() {
 		errors = append(errors, error(&parseError{err.String()}))
 	}
-	return ce, errors
+	ceResult.errors = errors
 }
 
-func loadFromSheets(client *http.Client, spreadsheetId string) []CostElement {
-	var ceSlice []CostElement
+func loadFromSheets(client *http.Client, spreadsheetId string) []CostElementResult {
+	var ceResults []CostElementResult
 	srv, err := sheets.New(client)
 	if err != nil {
 		log.Fatalf("Unable to retrieve Sheets client: %v", err)
@@ -245,21 +276,12 @@ func loadFromSheets(client *http.Client, spreadsheetId string) []CostElement {
 		log.Fatalf("Unable to retrieve data from sheet: %v", err)
 	}
 
-	for idx, sheet := range resp.Sheets {
+	for _, sheet := range resp.Sheets {
 		if sheet.Data != nil {
-			fmt.Printf("%d : %s, %s", idx, sheet.Properties.Title, sheet.Properties.SheetType)
-			ce, errors := handleSheet(sheet)
-			if errors == nil {
-				fmt.Printf(" %s valid", ce.Configuration.Name)
-			} else {
-				for _, err := range errors {
-					fmt.Printf("\n\t %v", err)
-				}
-			}
-			fmt.Printf("\n")
+			ceResults = append(ceResults, handleSheet(sheet))
 		}
 	}
-	return ceSlice
+	return ceResults
 }
 
 func loadKeycloakCreds() (keycloakCreds, error) {
@@ -271,32 +293,108 @@ func loadKeycloakCreds() (keycloakCreds, error) {
 	return kc, err
 }
 
-func main() {
-	b, err := ioutil.ReadFile("credentials.json")
-	if err != nil {
-		log.Fatalf("Unable to read client secret file: %v", err)
-	}
+func sendCostElement(ceResult CostElementResult, token oauth2.Token) CostElementResponse {
+	ceResponse := CostElementResponse{}
 
-	// If modifying these scopes, delete your previously saved token.json.
-	config, err := google.ConfigFromJSON(b, "https://www.googleapis.com/auth/spreadsheets.readonly")
-	if err != nil {
-		log.Fatalf("Unable to parse client secret file to config: %v", err)
+	cli := http.Client{
+		Timeout: time.Second * 5, // Maximum of 5 secs
 	}
-	client := getClient(config)
+	body, _ := json.Marshal(ceResult.costElement)
+	req, err := http.NewRequest(
+		http.MethodPost,
+		// fmt.Sprintf("https://controlpanel.%s/api/wf_dataset_service/v1/cost_elements", ceResult.env),
+		"http://localhost:9301/api/wf_dataset_service/v1/cost_elements"
+		bytes.NewBuffer(body))
 
-	// https://docs.google.com/spreadsheets/d/1Fcct8CdKHHqk_Ux53ddoA47aVQBxR9jYkzSGu8risLk/edit?ts=5d49dad4#gid=122535301
-	loadFromSheets(client, "1Fcct8CdKHHqk_Ux53ddoA47aVQBxR9jYkzSGu8risLk")
+	if err == nil {
+		req.URL.Query().Add("workforceId", ceResult.workforceId)
+		req.URL.Query().Add("actor", "00000000-0000-0000-0000-000000000000")
+		req.Header.Set("Content-Type", "application/json")
+		req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", token.AccessToken))
+		resp, reqerr := cli.Do(req)
+		if resp.StatusCode > 200 {
+			err = error(
+				&parseError{
+					fmt.Sprintf("Failed to create cost element for: %s\n\tcause: %s",
+						ceResult.sheetName, resp.Status)})
+		} else {
+			err = reqerr
+		}
+		if err == nil {
+			respBody, ioerr := ioutil.ReadAll(resp.Body)
+			err = ioerr
+			ceResponse.body = string(respBody)
+		}
+	}
+	ceResponse.err = err
 
-	kcCreds, err := loadKeycloakCreds()
-	if err != nil {
-		panic(err.Error())
+	return ceResponse
+}
+
+func sendCostElements(ceResults []CostElementResult, token oauth2.Token) []CostElementResponse {
+	var ceResps []CostElementResponse
+
+	for _, ceResult := range ceResults {
+		ceResps = append(ceResps, sendCostElement(ceResult, token))
 	}
-	fmt.Printf("%s : %s\n", kcCreds.User, kcCreds.Password)
-	tk, err := getKeycloakToken(kcCreds)
-	if err != nil {
-		panic(err.Error())
+	return ceResps
+}
+
+func dumpCeResults(ceResults []CostElementResult) {
+	for idx, ceResult := range ceResults {
+		fmt.Printf("%2d. %s workforceId: %s\t%s", idx, ceResult.env, ceResult.workforceId, ceResult.sheetName)
+		if ceResult.errors == nil {
+			fmt.Printf(" is valid")
+		} else {
+			for _, err := range ceResult.errors {
+				fmt.Printf(" is invalid")
+				fmt.Printf("\n\t %v", err)
+			}
+		}
+		fmt.Printf("\n")
 	}
+}
+
+func dumpToken(tk oauth2.Token) {
 	tkstr, _ := json.MarshalIndent(tk, "", "\t")
 	fmt.Printf(string(tkstr))
+}
 
+func dumpCeResponses(ceResponses []CostElementResponse) {
+	for idx, ceResp := range ceResponses {
+		if ceResp.err == nil {
+			fmt.Printf("%2d. succeess: %s\n", idx, ceResp.body)
+		} else {
+			fmt.Printf("%2d. failed: %v\n", idx, ceResp.err)
+		}
+	}
+}
+
+func main() {
+	client := getClient()
+	var sheetId string
+	var proceed string
+	fmt.Printf("enter the google sheet id you want to load, e.g. `13bAh82ug0zGIBkKJKlxIEa2SHFVceWvxVpuzF4Svfqk` in the example url below:\n")
+	fmt.Printf("https://docs.google.com/spreadsheets/d/13bAh82ug0zGIBkKJKlxIEa2SHFVceWvxVpuzF4Svfqk/edit#gid=0\n")
+	fmt.Printf("\n->  ")
+	fmt.Scan(&sheetId)
+	fmt.Println()
+
+	ceResults := loadFromSheets(client, sheetId)
+	dumpCeResults(ceResults)
+	fmt.Println("\n upload the valid cost elements? (y/N)")
+	fmt.Scan(&proceed)
+	fmt.Println()
+
+	if proceed == "y" {
+		kcCreds, err := loadKeycloakCreds()
+		if err != nil {
+			panic(err.Error())
+		}
+		tk, err := getKeycloakToken(kcCreds)
+		if err != nil {
+			panic(err.Error())
+		}
+		dumpCeResponses(sendCostElements(ceResults, tk))
+	}
 }
